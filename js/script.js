ad4bc85eecb41429b30dbb482a7a0fa1809524c5
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-20 11:44:57 +0200

    finished collision with other boxes collision

--- js/script.js
+++ js/script.js
@@ -4,22 +4,13 @@ let Player = document.getElementById('player');
 let x = 680;
 let y = 380;
 let computerOne = document.getElementById('computer-one');
-let xOne = 200;
-let yOne = 180;
 let playerLives = 3;
 
-console.log(playerLives);
-
-
-
-
 document.onkeydown = function(e) {
-
+//************************* */ moving the player ***********************
     if ( e.key == 'ArrowUp') {
         y -= 5;
         Player.style.marginTop = y +'px';
-        console.log(y)
-        console.log(x);
     }
     if ( e.key == 'ArrowRight') {
         x += 5;
@@ -33,7 +24,9 @@ document.onkeydown = function(e) {
         x -= 5;
         Player.style.marginLeft = x +'px';
     }
-    //   if the box touches the borders of the playground it resets the position of the box to the start point.
+
+    // *************** making the boxes move random ********************
+    //   ************** touch border recognition **************
     let Width = document.getElementById('play-ground').clientWidth - 30;
     let Height = document.getElementById('play-ground').clientHeight - 30;
     let target = document.getElementById('lives-target');
@@ -62,17 +55,138 @@ document.onkeydown = function(e) {
             x -= 5;
             Player.style.marginLeft = x + 'px';
         }
-        if(x == xOne && y == yOne) {
-            playerLives -= 1;
-            target.innerHTML = 'Lives left ' + playerLives + '/3';
-            console.log('touched');
 
-            
+
+
+// ****************variables for the 4 boxes and player for collision recognition***************
+        let boxOne = document.getElementById('computer-one').getBoundingClientRect();
+        let boxOneTop = boxOne.top;
+        let boxOneLeft = boxOne.left;
+        let boxOneRight = boxOne.right;
+        let boxOneBottom = boxOne.bottom;
+
+        let boxTwo = document.getElementById('computer-two').getBoundingClientRect();
+        let boxTwoTop = boxTwo.top;
+        let boxTwoLeft = boxTwo.left;
+        let boxTwoRight = boxTwo.right;
+        let boxTwoBottom = boxTwo.bottom;
+
+        let boxThree = document.getElementById('computer-three').getBoundingClientRect();
+        let boxThreeTop = boxThree.top;
+        let boxThreeLeft = boxThree.left;
+        let boxThreeRight = boxThree.right;
+        let boxThreeBottom = boxThree.bottom;
+
+        let boxFour = document.getElementById('computer-four').getBoundingClientRect();
+        let boxFourTop = boxFour.top;
+        let boxFourLeft = boxFour.left;
+        let boxFourRight = boxFour.right;
+        let boxFourBottom = boxFour.bottom;
+
+        let playerBox =  document.getElementById('player').getBoundingClientRect();
+        let playerBoxTop = playerBox.top;
+        let playerBoxLeft = playerBox.left;
+        let playerBoxRight = playerBox.right;
+        let playerBoxBottom = playerBox.bottom;
+
+
+            // ********************** vertical and horizontal collision recognition **********************
+
+            // box 1
+        let verticalMatchOne = false;
+        let horizontalMatchOne = false;
+        
+        if ((playerBoxTop > boxOneTop && playerBoxTop < boxOneBottom)||(playerBoxBottom > boxOneTop && playerBoxBottom < boxOneBottom)) {
+            verticalMatchOne = true
+        } else{
+            verticalMatchOne = false
+        }
+        if ((boxOneRight > playerBoxLeft && boxOneLeft < playerBoxRight) || (boxOneLeft > playerBoxLeft && boxOneRight < playerBoxRight)){
+            horizontalMatchOne = true;
+        }
+        else {
+            horizontalMatchOne = false;
+        }
+        // box 2
+        let verticalMatchTwo = false;
+        let horizontalMatchTwo = false;
+        if ((playerBoxTop > boxTwoTop && playerBoxTop < boxTwoBottom)|| (playerBoxBottom > boxTwoTop && playerBoxBottom < boxTwoBottom)) {
+            verticalMatchTwo = true
+        } else{
+            verticalMatchTwo = false
+        }
+        if ((boxTwoRight > playerBoxLeft && boxTwoLeft < playerBoxRight )|| (boxTwoLeft > playerBoxLeft && boxTwoRight < playerBoxRight)){
+            horizontalMatchTwo = true;
+        }
+        else {
+            horizontalMatchTwo = false;
+        }
+        // box 3
+        let verticalMatchThree = false;
+        let horizontalMatchThree = false;
+        if ((playerBoxTop > boxThreeTop && playerBoxTop < boxThreeBottom)||(playerBoxBottom > boxThreeTop && playerBoxBottom < boxThreeBottom)) {
+            verticalMatchThree = true
+        } else{
+            verticalMatchThree = false
+        }
+        if ((boxThreeRight > playerBoxLeft && boxThreeLeft < playerBoxRight) || (boxThreeLeft > playerBoxLeft && boxThreeRight < playerBoxRight)){
+            horizontalMatchThree = true;
+        }
+        else {
+            horizontalMatchThree = false;
+        }
+        // box 4
+        let verticalMatchFour = false;
+        let horizontalMatchFour = false;
+        if ((playerBoxTop > boxFourTop && playerBoxTop < boxFourBottom)||(playerBoxBottom > boxFourTop && playerBoxBottom < boxFourBottom)) {
+            verticalMatchFour = true
+        } else{
+            verticalMatchFour = false
+        }
+        if ((boxFourRight > playerBoxLeft && boxFourLeft < playerBoxRight )||( boxFourLeft > playerBoxLeft && boxFourRight < playerBoxRight)){
+            horizontalMatchFour = true;
+        }
+        else {
+            horizontalMatchFour = false;
         }
 
+            // ************************collision with box recognition*******************************
+        if (horizontalMatchOne == true && verticalMatchOne == true){
+            console.log('YEEEEETTT')
+            playerLives -= 1;
+            target.innerHTML = 'Lives left ' + playerLives + '/3';
+
+        }
+        if (horizontalMatchTwo == true && verticalMatchTwo == true){
+            console.log('YEEEEETTT')
+            playerLives -= 1;
+            target.innerHTML = 'Lives left ' + playerLives + '/3';
+
+        }
+        if (horizontalMatchThree == true && verticalMatchThree == true){
+            console.log('YEEEEETTT')
+            playerLives -= 1;
+            target.innerHTML = 'Lives left ' + playerLives + '/3';
+
+        }
+        if (horizontalMatchFour == true && verticalMatchFour == true){
+            console.log('YEEEEETTT')
+            playerLives -= 1;
+            target.innerHTML = 'Lives left ' + playerLives + '/3';
+
+        }
+            // ******************* restarting the game after death *****************************
         if( playerLives <= 0 ) {
-            alert('GameOver')
+            Player.style.color = 'yellow';
+            alert('GameOver!')
             location.reload();
+
+        }
+        else if (playerLives == 2){
+            Player.style.color = 'orange';
+        }
+        else if (playerLives == 1){
+            Player.style.color = 'red';
         }
 }
 
@@ -84,7 +198,7 @@ document.onkeydown = function(e) {
 
 
 
-
+        // ********** trying to make it move diagonally ***************
 
 
 
